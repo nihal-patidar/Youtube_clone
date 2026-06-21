cee827eafb307386bad7659c8ee77fc81780ed3d
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-21 21:31:02 +0530

    feat : define '/', '/vedio/:vedioId', '/channel/:channelId' routes in routes/router.jsx and export it

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,6 +1,12 @@
 import {createBrowserRouter} from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import Home from '../pages/Home'
+import VideoPlayer from '../pages/VideoPlayer'
+import Channel from '../pages/Channel'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Dashboard from '../pages/Dashboard'
+import NotFound from '../pages/NotFound'
 
 const routes = createBrowserRouter([
     {
@@ -10,9 +16,36 @@ const routes = createBrowserRouter([
             {
                 index : true,
                 element : <Home />
+            },
+            {
+                path : '/vedio/:vedioId',
+                element : <VideoPlayer />
+            },
+            {
+                path : '/channel/:channelId',
+                element : <Channel />
             }
         ],
 
 
+    },
+    {
+        path : '/login',
+        element : <Login />
+    },
+    {
+        path : '/register',
+        element : <Register />
+    },
+    {
+        path : '/dashboard',
+        element : <Dashboard />
+    },
+    {
+        path : '*',
+        element : <NotFound />
     }
 ])
+
+
+export default routes ;
