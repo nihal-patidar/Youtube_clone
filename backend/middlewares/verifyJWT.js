54d60cf3eab4025bac46a88a9d06cd0ce45d1bf1
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-24 19:40:37 +0530

    security : implement verifyJWT functional using access_token

---
+++ backend/middlewares/verifyJWT.js
@@ -0,0 +1,51 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const verifyJWT = async (
+  req,
+  res,
+  next
+) => {
+  try {
+
+    const token =
+      req.header("Authorization")
+      ?.replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(
+      decoded.id
+    ).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid Access Token",
+    });
+  }
+};
