b75dad43e37385af93117d4a7caee99b0cc8c92f
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-24 11:43:03 +0530

    chore : install and config .env(no tracking)

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
-const PORT = 3000 ;
+const PORT = process.env.PORT || 3000 ;
 
 server.listen(PORT , ()=>{
     console.log("Server is running on ", PORT);
