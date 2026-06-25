e4e88197f848627818c76d2c68dd86701fa73958
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-25 14:24:57 +0530

    feat : implement central error middleware for send error response

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,23 @@ import connectDB from "./config/db.js";
 import authRoutes from "./router.js";
 import router from "./router.js";
 import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.handler.js";
 dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 3000 ;
 
+connectDB();
+
 server.use(express.json());
 server.use(cookieParser());
 
-server.use(router)
 
-connectDB();
+
+server.use(router);
+
+server.use(errorMiddleware);
+
 
 server.listen(PORT , ()=>{
     console.log("Server is running on ", PORT);
