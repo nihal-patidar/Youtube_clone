9bf7f9861f0d4b465d2c3c52638ed056517a6c49
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-27 15:43:15 +0530

    refactor : change login action to loginSuccess in authSlice

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,31 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-// define initialState 
+// define initialState
 const initialState = {
-    user : null,
-    token : localStorage.getItem("youtube-user-token") || null,
-    isAuthenticated : !!localStorage.getItem("youtube-user-token")
-}
+  user: null,
+  token: localStorage.getItem("youtube-user-token") || null,
+  isAuthenticated: !!localStorage.getItem("youtube-user-token"),
+};
 
 const authSlice = createSlice({
-    name : "auth",
-    initialState,
+  name: "auth",
+  initialState,
 
-    reducers : {
-        login : (state,action)=>{
-            state.user = action.payload.user ;
-            state.token = action.payload.token ;
-            state.isAuthenticated = true ;
-        },
+  reducers: {
+    loginSuccess: (state, action) => {
+      console.log("loginSuccess ", action.payload);
+      state.user = action.payload.user;
+      state.token = action.payload.accessToken;
+      state.isAuthenticated = action.payload.success;
+    },
 
-        logout : (state,action)=>{
-            state.user = null;
-            state.token = null;
-            state.isAuthenticated = false ;
-        }
-    }
-})
+    logout: (state, action) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
 
-export const {login , logout} = authSlice.actions ;
-export default authSlice.reducer ;
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
