3fcce8c3082cf5d191f4e7e34a9b39485e278677
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-27 17:09:19 +0530

    feat : implement utility function access token

---
+++ frontend/src/utils/tokenStorage.js
@@ -0,0 +1,17 @@
+import appStore from "../redux/store";
+import {
+  setAccessToken as setAcceessTokenAction,
+  clearAccessToken as clearAccessTokenAction,
+} from "../redux/slices/authSlice";
+
+export const setAccessToken = (token) => {
+  appStore.dispatch(setAcceessTokenAction(token));
+};
+
+export const getAccessToken = () => {
+  return appStore.getState().auth.accessToken;
+};
+
+export const clearAccessToken = () => {
+  appStore.dispatch(setAcceessTokenAction());
+};
