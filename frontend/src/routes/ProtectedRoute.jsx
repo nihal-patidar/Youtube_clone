3372726d4b6c6db2f0d857d929ed1acf58449997
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-28 22:23:01 +0530

    feat : implement protected routes

---
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function ProtectedRoute({ children }) {
+  const user = useSelector((store) => store.auth.user);
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
