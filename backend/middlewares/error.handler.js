7bfdf778e98fea8946053b9da1e9f02f9d2c4afb
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-30 23:48:32 +0530

    fix : resolve error to create video

--- backend/middlewares/error.handler.js
+++ backend/middlewares/error.handler.js
@@ -1,8 +1,14 @@
+import ApiError from "../utils/ApiError.js";
+
 const errorMiddleware = (err, req, res, next) => {
-  return res.status(err.statusCode || 500).json({
-    success: false,
-    message: err.message,
-  });
+  // throw new err();
+
+  console.log("Error ", err);
+  // throw new ApiError(err.statusCode, err);
+  // return res.status(err.statusCode || 500).json({
+  //   success: false,
+  //   message: err.message,
+  // });
 };
 
 export default errorMiddleware;
--- backend/routes/video.routes.js
+++ backend/routes/video.routes.js
@@ -1,17 +1,38 @@
 import { Router } from "express";
-import { createVideo, deleteVideo, getAllVideos, getVideoById, updateVideo } from "../controllers/videoController.js";
+import {
+  createVideo,
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  updateVideo,
+} from "../controllers/videoController.js";
 import { verifyJWT } from "../middlewares/verifyJWT.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const videoRoutes = Router();
 
-videoRoutes.get('/all',getAllVideos);
+videoRoutes.get("/all", getAllVideos);
 
-videoRoutes.get('/:videoId',getVideoById);
+videoRoutes.get("/:videoId", getVideoById);
 
-videoRoutes.post('/',verifyJWT,createVideo);
+videoRoutes.post(
+  "/",
+  verifyJWT,
+  upload.fields([
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+    {
+      name: "video",
+      maxCount: 1,
+    },
+  ]),
+  createVideo,
+);
 
-videoRoutes.patch('/:videoId',verifyJWT,updateVideo);
+videoRoutes.patch("/:videoId", verifyJWT, updateVideo);
 
-videoRoutes.delete('/:videoId',verifyJWT,deleteVideo);
+videoRoutes.delete("/:videoId", verifyJWT, deleteVideo);
 
-export default videoRoutes ;
+export default videoRoutes;
